1ca75d0449baf88e23e3294b9626887ab52936b2
CozyWool <vsergeev201530@gmail.com>
2026-05-20 19:46:36 +0500

    upd ControlHelpText

---
+++ Assets/Scripts/VideoEventTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Events;
+
+public class VideoEventTrigger : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    // Твое событие, сюда в инспекторе можно накинуть функции
+    public UnityEvent onVideoFinished;
+
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+
+        // Подписка на событие конца видео
+        videoPlayer.loopPointReached += VideoEnded;
+
+        // Если видео весит много, лучше запустить подготовку
+        videoPlayer.Prepare();
+    }
+
+    void OnDestroy()
+    {
+        // Отписка, чтобы не было утечек памяти
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= VideoEnded;
+        }
+    }
+
+    private void VideoEnded(VideoPlayer source)
+    {
+
+        // Запуск твоего события
+        onVideoFinished?.Invoke();
+    }
+}
